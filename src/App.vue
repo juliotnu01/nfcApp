a5d2d6f17232a601bfce7029cbd16336a2b6fc4e
juliot nuñez <nunezjuliot@gmail.com>
2023-04-27 22:50:40 -0500

    commit temporal

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,16 @@
 
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { ref } from 'vue';
+
+
+const data = ref([
+  {text: ' hola', value: 1},
+  {text: ' mundo', value: 1},
+]);
+
+
+
+
+
 </script>
